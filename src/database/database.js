bd081a6177fc9a36aa5b5b32b5d0df3eaefa47c3
Danny Martínez <dannymg2002@gmail.com>
2024-02-19 09:48:16 -0500

    Backend 2

--- src/database/database.js
+++ src/database/database.js
@@ -5,7 +5,4 @@ const username = process.env.DB_USERNAME || "wearyou_user";
 const password = process.env.DB_PASSWORD || "";
 const host = process.env.DB_HOST || "localhost";
 
-export const sequelize = new Sequelize(database, username, password, {
-  host: host,
-  dialect: "postgres",
-});
+export const sequelize = new Sequelize(process.env.DATABASE_URL);
